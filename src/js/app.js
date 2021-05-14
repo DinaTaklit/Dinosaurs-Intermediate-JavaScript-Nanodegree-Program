b0f2b533bdd8a6f8bcbbe2b04fb892425cc6131a
DinaTaklit <ez_taklit@esi.dz>
2021-05-14 21:24:12 +0100

    feat: app: implement createDinoOnjects to create dinos objects

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,20 @@
-// Create Dino Constructor
+import Dino from './Dino'
 
-// Create Dino Objects
-
-
-// Create Human Object
-
-// Use IIFE to get human data from form
-
-
-// Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches. 
-
-
-// Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-// Generate Tiles for each Dino in Array
-
-    // Add tiles to DOM
-
-// Remove form from screen
-
-
-// On button click, prepare and display infographic
+/**
+ * @description Create dinos objects
+ * @param {json} dions The dinos object array
+ * @returns {array} The array of Dino instances
+ */
+export const createDinoOnjects = (dinos) =>
+    dinos.map(
+        (dino) =>
+            new Dino(
+                dino.species,
+                dino.weight,
+                dino.height,
+                dino.diet,
+                dino.where,
+                dino.when,
+                dino.fact
+            )
+    )
